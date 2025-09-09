2a51c5b4cdabd8190d70fc2c87b9db4fd48fd877
anilmisal2492 <anilmisal2492@gmail.com>
2025-09-09 16:25:48 +0530

    added validation and time

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,35 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  gender: String,
-  email: String,
-  password: String,
-  location: String,
-  confirmPassword: String,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 100,
+    },
+    gender: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    skills: [String],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
